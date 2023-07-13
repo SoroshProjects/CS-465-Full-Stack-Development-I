5d78dec73513e741b670c511f39b3742057f34c9
SoroshProjects <sorosh.khalili@snhu.edu>
2023-07-13 16:38:31 -0400

    Convert meals, rooms, and travel template from static data to dynamic JSON

--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,6 +1,9 @@
+const fs = require('fs');
+const room = JSON.parse(fs.readFileSync('./data/rooms.json', 'utf8'));
+
 /* GET Rooms View */
 const rooms = (req, res) => {
-    res.render('rooms', { title: 'Rooms - Travlr Getaways', activeTab: {rooms: true}});
+    res.render('rooms', { title: 'Rooms - Travlr Getaways', room, activeTab: {rooms: true}});
 };
 
 module.exports = {
