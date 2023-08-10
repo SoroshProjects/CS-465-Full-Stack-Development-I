3648e2d5a11f71e81e7450b9204e963c776da8dc
SoroshProjects <sorosh.khalili@snhu.edu>
2023-08-09 20:44:15 -0400

    Security

--- app_admin/src/app/add-trip/add-trip.component.ts
+++ app_admin/src/app/add-trip/add-trip.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { Router } from "@angular/router";
-import { TripDataService } from "../services/trip-data.service";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { TripDataService } from '../services/trip-data.service';
 
 @Component({
-  selector: "app-add-trip",
-  templateUrl: "./add-trip.component.html",
-  styleUrls: ["./add-trip.component.css"],
+  selector: 'app-add-trip',
+  templateUrl: './add-trip.component.html',
+  styleUrls: ['./add-trip.component.css'],
 })
 export class AddTripComponent implements OnInit {
   addForm: FormGroup;
@@ -16,19 +16,19 @@ export class AddTripComponent implements OnInit {
     private formBuilder: FormBuilder,
     private router: Router,
     private tripService: TripDataService
-  ) {}
+  ) { }
 
   ngOnInit() {
     this.addForm = this.formBuilder.group({
       _id: [],
-      code: ["", Validators.required],
-      name: ["", Validators.required],
-      length: ["", Validators.required],
-      start: ["", Validators.required],
-      resort: ["", Validators.required],
-      perPerson: ["", Validators.required],
-      image: ["", Validators.required],
-      description: ["", Validators.required],
+      code: ['', Validators.required],
+      name: ['', Validators.required],
+      length: ['', Validators.required],
+      start: ['', Validators.required],
+      resort: ['', Validators.required],
+      perPerson: ['', Validators.required],
+      image: ['', Validators.required],
+      description: ['', Validators.required],
     });
   }
 
@@ -37,13 +37,12 @@ export class AddTripComponent implements OnInit {
     if (this.addForm.valid) {
       this.tripService.addTrip(this.addForm.value).then((data) => {
         console.log(data);
-        this.router.navigate([""]);
+        this.router.navigate(['']);
       });
     }
   }
 
   // get the form short name to access the form fields
   get f() {
-    return this.addForm.controls;
-  }
+    return this.addForm.controls; }
 }
